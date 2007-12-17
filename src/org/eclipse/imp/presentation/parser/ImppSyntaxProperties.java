64ff68cb64be7d273459060dc54fec3f1c3c29da
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-12-17 18:52:35 +0000

    Implementation of new ILanguageSyntaxProperties interface.

---
+++ src/org/eclipse/imp/presentation/parser/ImppSyntaxProperties.java
@@ -0,0 +1,39 @@
+package org.eclipse.imp.presentation.parser;
+
+import org.eclipse.imp.language.ILanguageSyntaxProperties;
+
+public class ImppSyntaxProperties implements ILanguageSyntaxProperties {
+
+    public String getBlockCommentEnd() {
+        return null;
+    }
+
+    public String getBlockCommentStart() {
+        return null;
+    }
+
+    public String getSingleLineCommentPrefix() {
+        return "//";
+    }
+
+    public String getBlockCommentContinuation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String[][] getFences() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int[] getIdentifierComponents(String ident) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getIdentifierConstituentChars() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
